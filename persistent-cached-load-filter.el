cc68c0bf0c3fad8ed526a35fc151e7cd03b7cad7
include-yy <969041171@qq.com>
2025-11-25 06:12:29 +0800

    add more data structures

--- persistent-cached-load-filter.el
+++ persistent-cached-load-filter.el
@@ -42,7 +42,7 @@
 ;;   (persistent-cached-load-filter-easy-setup)
 ;;
 ;; Which is equivalent to:
-;; 
+;;
 ;;   (require 'persistent-cached-load-filter)
 ;;   (when (boundp 'load-path-filter-function)
 ;;     (setq load-path-filter-function #'persistent-cached-load-filter)
@@ -62,6 +62,13 @@
 ;; To write cache to file manually, use `persistent-cached-load-filter-write-cache'.
 ;; To clear cache, use `persistent-cached-load-filter-clear-cache'.
 ;;
+;; NOTE ON BENCHMARKING:
+;;
+;; You can change `persistent-cached-load-filter-assoc-type' to
+;; benchmark different data structures.  Note that if you change the
+;; type, you MUST clear the existing cache file before restarting Emacs,
+;; as the disk format changes depending on the chosen type.
+;;
 ;; TODO:
 ;;
 ;; * The current implementation shadows packages with the same name
@@ -73,6 +80,7 @@
 (require 'pcase)
 (require 'seq)
 (require 'radix-tree)
+(require 'map)
 
 (defconst t-filename "load-path-cache.eld"
   "Name of the cache file.")
@@ -83,7 +91,11 @@
 (defun t--read-cache ()
   "Read and return the contents of the load-path cache file.
 Return the Lisp object read from file.  Return nil if the file
-does not exist or if an error occurs during reading."
+does not exist or if an error occurs during reading.
+
+WARNING: If `persistent-cached-load-filter-assoc-type' has changed,
+the object returned here may not match the expected type, causing
+subsequent errors.  Clear the cache file if you change types."
   (when (file-exists-p t-cache-path)
     (ignore-errors
       (thread-first
@@ -92,11 +104,116 @@ does not exist or if an error occurs during reading."
           (buffer-string))
         read))))
 
-(defvar t--cache (or (t--read-cache) radix-tree-empty)
+(defvar t-assoc-type 'radix
+  "The data structure used for the in-memory cache.
+Must be set before loading the package.
+
+Valid values:
+
+hash+equal, alist+equal, plist+equal,
+hash+eq, alist+eq, plist+eq, radix.")
+
+(defun t--error-type ()
+  (error "Unknown associative array type: %s" t-assoc-type))
+
+(defalias 't--ini
+  (pcase t-assoc-type
+    ((or 'alist+eq 'alist+equal 'plist+eq 'plist+equal) (lambda () nil))
+    ('hash+eq (lambda () (make-hash-table)))
+    ('hash+equal (lambda () (make-hash-table :test #'equal)))
+    ('radix (lambda () radix-tree-empty))
+    (_ (t--error-type)))
+  "Initialize an empty cache container for the current type.")
+
+(defalias 't--elt
+  (pcase t-assoc-type
+    ('alist+eq (lambda (alist key) (alist-get (intern key) alist)))
+    ('alist+equal (lambda (alist key) (alist-get key alist nil nil #'equal)))
+    ('plist+eq (lambda (plist key) (plist-get plist (intern key))))
+    ('plist+equal (lambda (plist key) (plist-get plist key #'equal)))
+    ('hash+eq (lambda (ht key) (gethash (intern key) ht)))
+    ('hash+equal (lambda (ht key) (gethash key ht)))
+    ('radix (lambda (rt key) (radix-tree-lookup rt key)))
+    (_ (t--error-type)))
+  "Return the value associated with KEY in CONTAINER.
+KEY is always a string.
+
+\(fn CONTAINER KEY)")
+
+(defalias 't--del
+  (pcase t-assoc-type
+    ('alist+eq
+     (lambda (alist key)
+       (seq-remove (lambda (x) (eq (car x) (intern key))) alist)))
+    ('alist+equal
+     (lambda (alist key)
+       (seq-remove (lambda (x) (equal (car x) key)) alist)))
+    ('plist+eq
+     (lambda (plist key) (plist-put plist (intern key) nil)))
+    ('plist+equal
+     (lambda (plist key) (plist-put plist key nil #'equal)))
+    ('hash+eq (lambda (ht key) (remhash (intern key) ht) ht))
+    ('hash+equal (lambda (ht key) (remhash key ht) ht))
+    ('radix (lambda (rt key) (radix-tree-insert rt key nil)))
+    (_ (t--error-type)))
+  "Remove KEY from CONTAINER and return the updated container.
+KEY is always a string.
+
+\(fn CONTAINER KEY)")
+
+(defalias 't--put
+  (pcase t-assoc-type
+    ('alist+eq (lambda (alist key value) (push (cons (intern key) value) alist)))
+    ('alist+equal (lambda (alist key value) (push (cons key value) alist)))
+    ('plist+eq (lambda (plist key value) (plist-put plist (intern key) value)))
+    ('plist+equal (lambda (plist key value) (plist-put plist key value #'equal)))
+    ('hash+eq (lambda (ht key value) (puthash (intern key) value ht) ht))
+    ('hash+equal (lambda (ht key value) (puthash key value ht) ht))
+    ('radix (lambda (rt key value) (radix-tree-insert rt key value)))
+    (_ (t--error-type)))
+  "Associate KEY with VALUE in CONTAINER and return the updated container.
+KEY is always a string.
+
+\(fn CONTAINER KEY VALUE)")
+
+(defalias 't--map
+  (pcase t-assoc-type
+    ((or 'alist+eq 'plist+eq 'hash+eq)
+     (lambda (function eq-map)
+       (map-do (lambda (k v) (funcall function (symbol-name k) v)) eq-map)))
+    ((or 'alist+equal 'plist+equal 'hash+equal)
+     (lambda (function equal-map) (map-do function equal-map)))
+    ('radix (lambda (function rt) (radix-tree-iter-mappings rt function)))
+    (_ (t--error-type)))
+  "Iterate FUNCTION over CONTAINER.
+FUNCTION is called with (KEY VALUE), where KEY is a string.
+
+\(fn FUNCTION CONTAINER)")
+
+(defalias 't--from-alist
+  (let ((type (pcase t-assoc-type
+                ((or 'alist+eq 'alist+equal) 'alist)
+                ((or 'plist+eq 'plist+equal) 'plist)
+                ((or 'hash+eq 'hash+equal) 'hash-table)
+                ('radix 'radix)
+                (_ (t--error-type)))))
+    (pcase t-assoc-type
+      ((or 'alist+eq 'plist+eq 'hash+eq)
+       (lambda (alist)
+         (map-into (mapcar (pcase-lambda (`(,k . ,v))
+                             (cons (intern k) v))
+                           alist)
+                   type)))
+      ((or 'alist+equal 'plist+equal 'hash+equal)
+       (lambda (alist) (map-into alist type)))
+      ('radix (lambda (alist) (radix-tree-from-map alist)))
+      (_ (t--error-type))))
+  "Convert ALIST into a container of the current chosen type.")
+
+(defvar t--cache (or (t--read-cache) (t--ini))
   "In-memory cache for `load-path' filtering.
 
-This variable is a radix tree where keys are file name strings and
-values are lists of directories containing those files.
+The specific data structure depends on `persistent-cached-load-filter-assoc-type'.
 
 The value is initialized by reading from the disk cache file when
 this package is loaded.  This involves file I/O during startup.")
@@ -111,16 +228,15 @@ If FILE contains a directory component, return PATH unchanged.
 Otherwise, look up FILE in cache.
 
 If a cached value exists and contains only directories present in PATH,
-return it. If the cache is invalid or no entry is found, delegate to the
-default mechanism `load-path-filter-cache-directory-files'.
+return it.  If the cache is invalid or no entry is found, delegate to
+the default mechanism `load-path-filter-cache-directory-files'.
 
 If a new search is performed, add it to the cache for future use."
   (if (file-name-directory file) path
-    (let ((ls-cached (radix-tree-lookup t--cache file)))
+    (let ((ls-cached (t--elt t--cache file)))
       (when (not (seq-every-p (lambda (p) (member p path)) ls-cached))
         (setq t--need-update t)
-        ;; Removing the key by inserting nil
-        (setq t--cache (radix-tree-insert t--cache file nil))
+        (setq t--cache (t--del t--cache file))
         (setq ls-cached nil))
       (or ls-cached
           (let ((ls (load-path-filter-cache-directory-files path file suf)))
@@ -129,12 +245,12 @@ If a new search is performed, add it to the cache for future use."
             (if (eq path ls) path
               (when ls
                 (setq t--need-update t)
-                (setq t--cache (radix-tree-insert t--cache file ls))
+                (setq t--cache (t--put t--cache file ls))
                 ls)))))))
 
 (defun t--try-uniquify-cache ()
   "Remove duplicates and non-existent files from the cache.
-Return the cleaned radix tree.
+Return the cleaned cache structure.
 
 This function also performs string interning on directory paths to
 reduce memory usage and disk footprint."
@@ -142,8 +258,7 @@ reduce memory usage and disk footprint."
          ;; Helper hash table for string interning
          (ht (make-hash-table :test #'equal))
          (alist nil))
-    (radix-tree-iter-mappings
-     t--cache
+    (t--map
      (lambda (name paths)
        ;; Actually we use `locate-file' here, so the shadowed path will be ignored.
        (when-let* ((file (locate-file name paths suffixes))
@@ -151,9 +266,11 @@ reduce memory usage and disk footprint."
                    ;; Intern the directory string.
                    (unique-dir (with-memoization (gethash dir ht)
                                  (puthash dir dir ht))))
-         (push (cons name (list unique-dir)) alist))))
-    (radix-tree-from-map alist)))
-                              
+         (push (cons name (list unique-dir)) alist)))
+     t--cache)
+    (t--from-alist alist)))
+
+
 (defun t-write-cache ()
   "Write the uniquified load path cache to disk.
 Filter cache to remove duplicates and entries for files that
@@ -162,7 +279,7 @@ no longer exist, then write the result to
 
 Do nothing if `persistent-cached-load-filter--need-update' is nil."
   (interactive)
-  (when (and t--cache t--need-update)
+  (when t--need-update
     (let ((print-circle t))
       (with-temp-file t-cache-path
         (pp (t--try-uniquify-cache) (current-buffer))))))
@@ -171,10 +288,10 @@ Do nothing if `persistent-cached-load-filter--need-update' is nil."
   "Clear the content of the persistent load path cache file.
 This resets both the cache file on disk and the in-memory variable."
   (interactive)
-  (setq t--cache radix-tree-empty)
+  (setq t--cache (t--ini))
   (setq t--need-update nil)
   (with-temp-file t-cache-path
-    (prin1 radix-tree-empty (current-buffer))))
+    (prin1 t--cache (current-buffer))))
 
 (defun t-easy-setup ()
   "Configure the persistent load path cache.
